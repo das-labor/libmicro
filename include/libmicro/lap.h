1c0d2bf7967c13b83f32f0d01b006d98a515f0dc
fridtjof <2780577+fridtjof@users.noreply.github.com>
2020-02-06 23:18:20 +0100

    lap.h: format + a little explanation

--- include/libmicro/lap.h
+++ include/libmicro/lap.h
@@ -13,28 +13,31 @@
  * Types
  */
 
+// """ServiceDataObject"""
 // "inherits" from can_message
 typedef struct {
-        can_addr addr_src;
-        can_addr addr_dst;
-        can_port port_src;
-        can_port port_dst;
-        unsigned char dlc;
-        unsigned char cmd;
-        uint16_t index;
-        uint16_t size;
-        uint16_t address;
+    can_addr addr_src;
+    can_addr addr_dst;
+    can_port port_src;
+    can_port port_dst;
+    unsigned char dlc;
+    unsigned char cmd;
+    uint16_t index;
+    uint16_t size;
+    uint16_t address;
 } sdo_message;
 
+// """ProcessDataObject"""???
+// basically command (FKT_ enum werte) + funktionsspezifische daten
 // "inherits" from can_message
 typedef struct {
-        can_addr addr_src;
-        can_addr addr_dst;
-        can_port port_src;
-        can_port port_dst;
-        unsigned char dlc;
-        unsigned char cmd;
-        unsigned char data[7];
+    can_addr addr_src;
+    can_addr addr_dst;
+    can_port port_src;
+    can_port port_dst;
+    unsigned char dlc;
+    unsigned char cmd;
+    unsigned char data[7];
 } pdo_message;
 
 /****************************************************************************
@@ -42,26 +45,36 @@ typedef struct {
  */
 
 // not sure about PORT_LAMPE. it used to be 0x20, which did not work. But who knows which weird devices use 0x20 for this
-typedef enum { PORT_MGT = 0x30, PORT_LAMPE = 0x02, PORT_SDO = 0x15, PORT_SDO_DATA = 0x16, PORT_LAPD = 0x18,
-               PORT_BORG = 0x23, PORT_MOOD = 0x17, PORT_REMOTE = 0x21, PORT_GATE = 0x22, PORT_CHUCK = 0x26
-             }  ports;
+typedef enum {
+    PORT_MGT = 0x30, PORT_LAMPE = 0x02, PORT_SDO = 0x15, PORT_SDO_DATA = 0x16, PORT_LAPD = 0x18,
+    PORT_BORG = 0x23, PORT_MOOD = 0x17, PORT_REMOTE = 0x21, PORT_GATE = 0x22, PORT_CHUCK = 0x26
+} ports;
 
-typedef enum { FKT_MGT_PING = 0x00, FKT_MGT_PONG = 0x01,
-               FKT_MGT_RESET = 0x02, FKT_MGT_AWAKE = 0x03, FKT_MGT_TIMEREQUEST = 0x04, FKT_MGT_TIMEREPLY = 0x05
-             }      lap_mgt_fkts;
+// FunKTionen, nach Ports
 
-typedef enum { FKT_LAMPE_SET = 0x00, FKT_LAMPE_SETMASK = 0x01,
-               FKT_LAMPE_SETDELAY = 0x02, FKT_LAMPE_ADD = 0x03
-             }                   lap_lampe_fkts;
+typedef enum {
+    FKT_MGT_PING = 0x00, FKT_MGT_PONG = 0x01,
+    FKT_MGT_RESET = 0x02, FKT_MGT_AWAKE = 0x03, FKT_MGT_TIMEREQUEST = 0x04, FKT_MGT_TIMEREPLY = 0x05
+} lap_mgt_fkts;
 
-typedef enum { FKT_BORG_INFO = 0x00, FKT_BORG_MODE = 0x01, FKT_BORG_SCROLLTEXT_RESET = 0x02,
-               FKT_BORG_SCROLLTEXT_APPEND = 0x03
-             } lap_borg_fkts;
+typedef enum {
+    FKT_LAMPE_SET = 0x00, FKT_LAMPE_SETMASK = 0x01,
+    FKT_LAMPE_SETDELAY = 0x02, FKT_LAMPE_ADD = 0x03
+} lap_lampe_fkts;
 
-typedef enum { FKT_ONOFF_INFO = 0, FKT_ONOFF_SET = 1, FKT_ONOFF_GET = 2,
-             } lap_lapd_fkts;
+typedef enum {
+    FKT_BORG_INFO = 0x00, FKT_BORG_MODE = 0x01, FKT_BORG_SCROLLTEXT_RESET = 0x02,
+    FKT_BORG_SCROLLTEXT_APPEND = 0x03
+} lap_borg_fkts;
 
-typedef enum { FKT_MOOD_INFO = 0x00, FKT_MOOD_GET = 0x01, FKT_MOOD_SET = 0x02, FKT_MOOD_ONOFF = 0x03} lap_mood_fkts;
+typedef enum {
+    FKT_ONOFF_INFO = 0, FKT_ONOFF_SET = 1, FKT_ONOFF_GET = 2,
+} lap_lapd_fkts;
+
+typedef enum {
+    FKT_MOOD_INFO = 0x00, FKT_MOOD_GET = 0x01, FKT_MOOD_SET = 0x02, FKT_MOOD_ONOFF = 0x03
+} lap_mood_fkts;
+
 #define SDO_CMD_READ            0x20
 #define SDO_CMD_REPLY           0x21
 #define SDO_CMD_INFO            0x22
@@ -89,10 +102,10 @@ typedef enum { FKT_MOOD_INFO = 0x00, FKT_MOOD_GET = 0x01, FKT_MOOD_SET = 0x02, F
  */
 
 // send ping to dst
-void lap_ping( can_addr dst );
+void lap_ping(can_addr dst);
 
 // send reset request to dst
-void lap_reset( can_addr dst );
+void lap_reset(can_addr dst);
 
 #ifdef LAP_TIME_EXTENSION
 //variables to save the last received hours and  minutes
